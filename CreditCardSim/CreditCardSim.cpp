7e8d3fc710945759560fbe153685bb4e6ab198f1
Erin K. Kennon <ekkennon@gmail.com>
2017-07-12 19:04:02 -0500

    initial commit

---
+++ CreditCardSim/CreditCardSim.cpp
@@ -0,0 +1,11 @@
+// CreditCardSim.cpp : main project file.  Erin K Kennon
+
+#include "stdafx.h"
+
+using namespace System;
+
+int main()
+{
+    
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Credit Card Simulator
+=====================
+Project 6 from c++ class at STLCC.
